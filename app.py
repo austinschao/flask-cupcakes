b89ac77a60e1e834c9fd8c4008c8812104cdd558
Austin Chao <austinschao@gmail.com>
2022-04-04 12:33:29 -0700

    Part One and Part 2 KeyError in Insomnia POST

--- app.py
+++ app.py
@@ -1,1 +1,89 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, jsonify, request
+
+# from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+db.create_all()
+
+
+
+
+@app.get("/api/cupcakes")
+def list_all_cupcakes():
+    """ Return JSON of all cupcakes
+
+        {"cupcakes": [
+            {
+                "id",
+                "flavor",
+                "size",
+                "rating",
+                "image"
+            }
+        ]}
+     """
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.get("/api/cupcakes/<int:cupcake_id>")
+def list_single_cupcake(cupcake_id):
+    """ Return JSON of a single cupcake
+
+        {"cupcake":
+            {
+                "id",
+                "flavor",
+                "size",
+                "rating",
+                "image"
+            }
+        }
+     """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """ Create a cupcake from form data and return JSON of it
+
+        {"cupcake":
+            {
+                "id",
+                "flavor",
+                "size",
+                "rating",
+                "image"
+            }
+        }
+    """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
--- models.py
+++ models.py
@@ -1,1 +1,44 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+DEFAULT_IMG_URL = "https://tinyurl.com/demo-cupcake"
+
+db = SQLAlchemy()
+
+class Cupcake(db.Model):
+    """ Create a cupcake """
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    flavor = db.Column(db.String(30), nullable=False)
+
+    size = db.Column(db.Text, nullable=False)
+
+    rating = db.Column(db.Integer, nullable=False)
+
+    image = db.Column(db.Text, nullable=False, default = DEFAULT_IMG_URL)
+
+    def serialize(self):
+        """ Serialize to dictionary """
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image
+        }
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
